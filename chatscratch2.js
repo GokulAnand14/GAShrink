958435d893210d2aaa2b7c88d4de740844c084c1
GA's Lab <125134406+GokulAnand14@users.noreply.github.com>
2023-05-02 15:04:13 +0530

    Update and rename chatscratch1.js to chatscratch2.js

--- chatscratch2.js
+++ chatscratch2.js
@@ -24,7 +24,7 @@ class AIBlock {
         //Remove trailing spaces, required for model to work properly
         const text = string.trim();
         //Request text completion using Davinci3
-        const url = `https://api.openai.com/v1/engines/tex...`;
+        const url = `https://api.openai.com/v1/completions/`;
 
         const options = {
             //Has to be post for some reason
